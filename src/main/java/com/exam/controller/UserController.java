03fb600d09c8729667812359a7363cac5a6c6a4b
bulbul-deploy <bulbulahmed.cs13@gmail.com>
2021-05-05 11:21:45 +0600

    Send Data From API

--- src/main/java/com/exam/controller/UserController.java
+++ src/main/java/com/exam/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     //creating user
     @PostMapping("/")
     public User createUser(@RequestBody User user) throws Exception {
-
+//
         user.setProfile("default.png");
         Set<UserRole> roles = new HashSet<>();
         Role role = new Role();
